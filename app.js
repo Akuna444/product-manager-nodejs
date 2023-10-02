ddc0d7f273a758bbb94e128b503ae09fa6677177
Akuna444 <aklilsolomon3@gmail.com>
2023-10-02 14:09:45 +0300

    add: neccessary routes

---
+++ app.js
@@ -0,0 +1,25 @@
+const path = require("path");
+
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.set("view engine", "ejs");
+app.set("views", "views");
+
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+const pageNotFoundControllers = require("./controllers/404.js");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/admin", adminRoutes.routes);
+app.use(shopRoutes);
+
+app.use(pageNotFoundControllers.pageNotFound);
+
+app.listen(3000, () => {
+  console.log("Started on 3000");
+});
