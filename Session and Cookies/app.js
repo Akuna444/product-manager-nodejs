256cb9e455f995a4cf57463565676642839ab44a
Akuna444 <aklilsolomon3@gmail.com>
2023-11-24 10:47:54 +0300

    update: session and cookies folder

--- Session and Cookies/app.js
+++ Session and Cookies/app.js
@@ -1,78 +1,56 @@
-const path = require("path");
+const path = require('path');
 
-const express = require("express");
-const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
-const session = require("express-session");
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
-const errorController = require("./controllers/error");
-const User = require("./models/user");
-const MongoStore = require("connect-mongodb-session")(session);
-const MONGODB_URI =
-  "mongodb+srv://akuna444:lYlyqPjkwCsbU51A@cluster0.ex41jje.mongodb.net/shop";
-
-const store = new MongoStore({
-  uri: MONGODB_URI,
-  collection: "sessions",
-});
+const errorController = require('./controllers/error');
+const User = require('./models/user');
 
 const app = express();
 
-app.set("view engine", "ejs");
-app.set("views", "views");
+app.set('view engine', 'ejs');
+app.set('views', 'views');
 
-const adminRoutes = require("./routes/admin");
-const shopRoutes = require("./routes/shop");
-const authRoutes = require("./routes/auth");
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, "public")));
-app.use(
-  session({
-    secret: "my secret",
-    resave: false,
-    saveUninitialized: false,
-    store: store,
-  })
-);
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
-  console.log(req.session.user);
-  if (!req.session.user) {
-    return next();
-  }
-  User.findById(req.session.user._id)
-    .then((user) => {
+  User.findById('5bab316ce0a7c75f783cb8a8')
+    .then(user => {
       req.user = user;
       next();
     })
-    .catch((err) => console.log(err));
+    .catch(err => console.log(err));
 });
 
-app.use("/admin", adminRoutes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
-app.use(authRoutes);
 
 app.use(errorController.get404);
 
 mongoose
-  .connect(MONGODB_URI)
-  .then((result) => {
-    User.findOne().then((user) => {
+  .connect(
+    'mongodb+srv://maximilian:9u4biljMQc4jjqbe@cluster0-ntrwp.mongodb.net/shop?retryWrites=true'
+  )
+  .then(result => {
+    User.findOne().then(user => {
       if (!user) {
         const user = new User({
-          name: "Max",
-          email: "max@test.com",
+          name: 'Max',
+          email: 'max@test.com',
           cart: {
-            items: [],
-          },
+            items: []
+          }
         });
         user.save();
       }
     });
     app.listen(3000);
-    console.log("App started successfully");
   })
-  .catch((err) => {
+  .catch(err => {
     console.log(err);
   });
