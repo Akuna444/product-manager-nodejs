3ce99d4939b08bf10aaeb2e0187ef2a4311a5c53
Akuna444 <aklilsolomon3@gmail.com>
2023-11-29 11:45:10 +0300

    add: validation to editing product

--- input-validation/controllers/admin.js
+++ input-validation/controllers/admin.js
@@ -21,9 +21,8 @@ exports.postAddProduct = (req, res, next) => {
   const errors = validationResult(req);
 
   if (!errors.isEmpty()) {
-    console.log(errors.array());
     return res.status(422).render("admin/edit-product", {
-      pageTitle: "Edit Product",
+      pageTitle: "Add Product",
       path: "/admin/edit-product",
       editing: false,
       hasError: true,
@@ -73,6 +72,7 @@ exports.getEditProduct = (req, res, next) => {
         hasError: false,
         product: product,
         isAuthenticated: req.session.isLoggedIn,
+        errorMessage: null,
       });
     })
     .catch((err) => console.log(err));
@@ -84,6 +84,24 @@ exports.postEditProduct = (req, res, next) => {
   const updatedPrice = req.body.price;
   const updatedImageUrl = req.body.imageUrl;
   const updatedDesc = req.body.description;
+
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(422).render("admin/edit-product", {
+      pageTitle: "Edit Product",
+      path: "/admin/edit-product",
+      editing: true,
+      hasError: true,
+      product: {
+        title: updatedTitle,
+        imageUrl: updatedImageUrl,
+        price: updatedPrice,
+        description: updatedDesc,
+      },
+      errorMessage: errors.array()[0].msg,
+    });
+  }
 
   Product.findById(prodId)
     .then((product) => {
