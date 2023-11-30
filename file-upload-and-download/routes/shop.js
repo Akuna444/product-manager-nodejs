c829a7bb305fc4022468e3e213ec51416b2a4dd8
Akuna444 <aklilsolomon3@gmail.com>
2023-11-30 21:20:45 +0300

    add: fetch invoices functionality

--- file-upload-and-download/routes/shop.js
+++ file-upload-and-download/routes/shop.js
@@ -21,5 +21,6 @@ router.post("/cart-delete-item", isAuth, shopController.postCartDeleteProduct);
 router.post("/create-order", isAuth, shopController.postOrder);
 
 router.get("/orders", isAuth, shopController.getOrders);
+router.get("/order/getInvoice", isAuth, shopController.getInvoices)
 
 module.exports = router;
