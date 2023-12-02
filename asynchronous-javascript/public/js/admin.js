832a42ea1559ebd38a2773cdc3597a7f79be208a
Akuna444 <aklilsolomon3@gmail.com>
2023-12-02 10:17:32 +0300

    delete: product with DOM manipulation

--- asynchronous-javascript/public/js/admin.js
+++ asynchronous-javascript/public/js/admin.js
@@ -2,12 +2,18 @@ function deleteProduct(btn) {
   const prodId = btn.parnetNode.querySelector("name=productId").value;
   const csrf = btn.parnetNode.querySelector("name=_csrf").value;
 
+  const productElement = btn.closest("article");
+
   fetch(`/admin/product/${prodId}`, {
     method: "DELETE",
     headers: {
       "csrf-token": csrf,
     },
   })
-    .then((result) => console.log(result))
+    .then((result) => result.json())
+    .then((data) => {
+      console.log(data);
+      productElement.parnetNode.removeChild(productElement);
+    })
     .catch((err) => console.log(err));
 }
